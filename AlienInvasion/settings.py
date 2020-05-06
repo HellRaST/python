805354c104fb9a91c8054dea6ac232aa3fdb9123
HellRaST <64853385+HellRaST@users.noreply.github.com>
2020-05-06 13:20:08 +0300

    update1.2

--- AlienInvasion/settings.py
+++ AlienInvasion/settings.py
@@ -8,4 +8,10 @@ class Settings():
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
         # Настройка корабля
-        self.ship_speed = 1
+        self.ship_speed = 1.5
+        # Параметры снаряда
+        self.bullet_speed = 1
+        self.bullet_width = 1
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
